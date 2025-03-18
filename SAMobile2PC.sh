36ca98629528f177ccbb0e95ab5a5611f61280d6
CharaGFX (GSFGANGSTER27) <105109442+GSFGANGSTER27@users.noreply.github.com>
2025-03-18 19:17:43 +0700

    Add files via upload

---
+++ SAMobile2PC.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/dash
+# Converts San Andreas Mobile DFF to PC
+
+# Converting...
+cd "/storage/emulated/0/gtastuff/librwgta/ViceCity"
+for i in *.dff
+    do convdff -u -o d3d8 "$i" "out/${i%.*}.dff"
+done
+
+# Removing unconverted DFFs to avoid irritations
+for i in *.dff
+    do rm "$i"
+done
+
+cd
